acbcf38793dc7e893dcb0c4f95c975c3fb10909b
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2020-09-16 16:47:30 +1200

    changes to fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './Components/Home';
 import Login from './Components/Login';
-import Adventures from './Components/Login';
+import Adventures from './Components/Adventures';
 import Gallery from './Components/Gallery';
 import Maps from './Components/Maps';
 import NoMatch from './Components/NoMatch';
-class App extends Component{
-  render(){
-    return(
-      <React.Fragment>
+import Nav from './Components/Navbar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
         <Router>
           <Switch>
-          <Route eact path="/" component ={Home}/>
-          <Route path="/login" component ={Login}/>
-          <Route path="/adventures" component ={Adventures}/>
-          <Route path="/gallery" component ={Gallery}/>
-          <Route path="/map" component ={Maps}/>
-          <Route component={NoMatch} />
-        
-
-          </Switch>
+            <Route path="/" component ={Nav} />
+            <Route exact path="/" component ={Home} />
+            <Route exact path="/login" component ={Login} />
+            <Route exact path="/adventures" component ={Adventures} />
+           <Route exact path="/gallery" component ={Gallery} />
+            <Route  exact path="/map" component ={Maps} />
+            <Route component={NoMatch} />
+            </Switch>
         </Router>
-
-      </React.Fragment>
+      </Fragment>
     );
   }
 
