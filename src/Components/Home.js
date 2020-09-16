c4f7c92fdea34d9364f2d481415bd95f54b671d6
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2020-09-16 22:33:55 +1200

    add jumbotron

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-
+import Jumbotron from 'react-bootstrap/Jumbotron';
 const Home = () => (
     
-   
+   <Jumbotron fluid>
         <Container>
             
             <h1 className='home-title'>This is Home</h1>
             <p>I have been working on thiis for too long </p>
         </Container>
-    
+        
+        </Jumbotron>
 )
 
 export default Home;
